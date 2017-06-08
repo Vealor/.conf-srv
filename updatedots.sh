45e3b622a48cc2c7150bf2434bfed5f40b2e4ae6
Vealor <jakob.m.roberts@gmail.com>
2017-06-08 17:38:18 +0900

    Huge update, made create, need restore & have to test lol

--- updatedots.sh
+++ updatedots.sh
@@ -1,28 +1,174 @@
-########## Variables
-dir=~/.dotfiles                    # dotfiles directory
-olddir=~/.dotfiles_old             # old dotfiles backup directory
-files=".gitconfig .bashrc .bash_aliases .tmux.conf .vimrc .moc"        # list of files/folders to symlink in homedir 
-##########
+#!/bin/bash
+################################################################################
+# Create dotfiles on new system as per Veal's spec.
+# Has two usages.
+#
+# Usage:
+#   bash updatedots.sh create  #create/update dotfile symlinks
+#   bash updatedots.sh restore #restores pre-existing dotfiles for system
+#
+################################################################################
+# list of files/folders to symlink in homedir
+FILES=".gitconfig .bashrc .bash_aliases .tmux.conf .vimrc .moc"
 
- 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
- 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
- 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/.dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
-done
+DIR=~/.dotfiles         # dotfiles directory
+OLDDIR=~/.dotfiles_old  # old dotfiles backup directory
 
-# reload
+GOOD="[\e[92mGOOD\e[39m]"
+PASS="[\e[93mPASS\e[39m]"
+FAIL="[\e[31mFAIL\e[39m]"
+################################################################################
+################################################################################
+# Helper function for loading
+#   $1 - process ID, ususally last one  [ $! ]
+#   $2 - input text
+#   $3 - if there is an initial input tab to be displayed (0=no, else yes)
+function spinny {
+  local PID=$1
+  local TEXT=$2
+  local TAB=$3 #if initial tab or not
+  local TEXT_LEN=${#TEXT}
+  local DELAY=0.7
+  local FRAMES='|/-\'
+  while [ -d /proc/$PID ];
+  do
+    if [ "$TAB" -eq 0 ] ; then
+      printf "[%c%c%c%c]\t%s" "$FRAMES" "$FRAMES" "$FRAMES" "$FRAMES" "$TEXT"
+    else
+      printf "     [%c%c%c%c]\t%s" "$FRAMES" "$FRAMES" "$FRAMES" "$FRAMES" "$TEXT"
+    fi
+    local TMP=${FRAMES#?}
+    FRAMES=$TMP${FRAMES%"$TMP"}
+    sleep $DELAY
+    if [ "$TAB" -eq 0 ] ; then
+      printf "\b\b\b\b\b\b\b\b\b"
+    else
+      printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+    fi
+    local BEGIN=0
+    for ((i=$BEGIN;i<$TEXT_LEN;i++)); do
+      printf "\b"
+    done
+  done
+}
+################################################################################
+# Helper function to change directory
+#   $1 - directory location
+function changedir {
+  #first in variable is location of directory change
+  local THEDIR=$1
+  local TEXT="Changing to the $THEDIR directory"
+
+  (sleep 1) &
+  #the $! gets last process ID
+  spinny $! $TEXT
+
+  #used hyphen to make out not empty on success
+  #main output for function
+  local OUT="-$(cd $THEDIR 2>&1)"
+  #status icon for function running
+  spinny $! $TEXT
+
+  #display good/pass/fail output
+  if [[ $OUT == *No\ such* ]]; then #if folder does not exist
+    echo -e "$FAIL\t$TEXT"
+    exit 1
+  elif [[ $OUT == *[!\ ]* ]]; then #only hyphen
+    echo -e "$GOOD\t$TEXT"
+  else #any other output that contains a space
+    echo -e "$FAIL\t$TEXT"
+    exit 1
+  fi
+}
+################################################################################
+# Create backup directory for existing dotfiles
+function makeolddir {
+  local TEXT="Creating $OLDDIR for backup of any existing dotfiles in ~/"
+
+  (sleep 1) &
+  spinny $! $TEXT
+
+  #used hyphen to make out not empty on success
+  #main output for function
+  local OUT="-$(mkdir $OLDDIR 2>&1 &)"
+  #status icon for function running
+  spinny $! $TEXT
+
+  #display good/pass/fail output
+  if [[ $OUT == *File\ exists* ]]; then #if folder exists
+    echo -e "$PASS\t$TEXT"
+  elif [[ $OUT == *[!\ ]* ]]; then #only hyphen
+    echo -e "$GOOD\t$TEXT"
+  else #any other output that contains a space
+    echo -e "$FAIL\t$TEXT"
+    exit 1
+  fi
+}
+################################################################################
+# creates symlinks for dotfiles
+function create {
+
+  # move existing dotfiles in DIR to OLDDIR directory
+  local TEXT="Moving any existing dotfiles from ~ to $OLDDIR"
+  echo -e "[||||]\t$TEXT"
+  for FILE in $FILES; do
+      local OUT="-$(mv ~/$FILE ~/.dotfiles_old/ &)"
+      spinny $! $FILE 1
+      #display good/pass/fail output
+      if [[ $OUT == *File\ exists* ]]; then #if a symlink exists
+        echo -e "     $PASS\t$FILE"
+      elif [[ $OUT == *[!\ ]* ]]; then #only hyphen
+        echo -e "     $GOOD\t$FILET"
+      else #any other output that contains a space
+        echo -e "     $FAIL\t$FILE"
+        exit 1
+      fi
+  done
+
+
+  #reset output var
+  local OUT=""
+
+  # create symlinks for new dotfiles in DIR
+  # 3 seconds to complete
+  (sleep 3) &
+  local PID=$!
+  local TEXT="Creating symlink to $FILE in home directory."
+  for FILE in $FILES; do
+      local OUT="-$(ln -s $DIR/$FILE ~/$FILE &)"
+      spinny $! $FILE 1
+      #display good/pass/fail output
+      if [[ $OUT == *File\ exists* ]]; then #if a symlink exists
+        echo -e "     $PASS\t$FILE"
+      elif [[ $OUT == *[!\ ]* ]]; then #only hyphen
+        echo -e "     $GOOD\t$FILET"
+      else #any other output that contains a space
+        echo -e "     $FAIL\t$FILE"
+        exit 1
+      fi
+  done
+}
+################################################################################
+################################################################################
+# main body
+# Usage:
+#   bash updatedots.sh create  #create/update dotfile symlinks
+#   bash updatedots.sh restore #restores pre-existing dotfiles for system
+
+MODE=$1
+if [ "$MODE" == "create" ]; then
+  #do stuff
+  echo -e "create"
+elif [ "$MODE" == "restore" ]; then
+  #do other stuff
+  echo -e "restore"
+else
+  echo -e "~~~ \e[33mUSAGE:\e[39m ~~~"
+  echo -e "\e[34mbash updatedots.sh create  # create/update dotfile symlinks"
+  echo -e "\e[35mbash updatedots.sh restore # restores pre-existing dotfiles for system\e[39m"
+fi
+
+#reload configurations
 source ~/.bashrc
 #source ~/.vimrc
+################################################################################
